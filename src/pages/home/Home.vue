9f4986c7108ecbd1b3c49b68873a6a3a81605fa4
shiyvmou <2630663372@qq.com>
2019-09-29 15:43:39 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>hello</div>
+  <div>
+      <homeHeader></HomeHeader>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  },
   data () {
     return {}
   },
